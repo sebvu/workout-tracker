780edf7ed711172d740f2c4780da5f52ccd1272d
sebvu <santosjester738@gmail.com>
2024-12-20 15:02:32 -0600

    Reformat and update InvalidConfigurationFile oput

--- src/exceptions.cpp
+++ src/exceptions.cpp
@@ -3,18 +3,33 @@
 
 // invalid argument count
 
-InvalidArgumentCount::InvalidArgumentCount(int argc) { message = "Invalid argument count: " + std::to_string(argc) + "\nExpect 2 arguments."; }
+InvalidArgumentCount::InvalidArgumentCount(int argc) {
+  message =
+    "Invalid argument count: " + std::to_string(argc) + "\nExpect 2 arguments.";
+}
 
-const char *InvalidArgumentCount::what() const noexcept { return message.c_str(); }
+const char *InvalidArgumentCount::what() const noexcept {
+  return message.c_str();
+}
 
 // invalid argument usage
 
-InvalidArgumentUsage::InvalidArgumentUsage(const std::string &arg) { message = "Argument " + arg + " is invalid.\nExpect config.toml"; }
+InvalidArgumentUsage::InvalidArgumentUsage(const std::string &arg) {
+  message = "Argument " + arg + " is invalid.\nExpect config.toml";
+}
 
-const char *InvalidArgumentUsage::what() const noexcept { return message.c_str(); }
+const char *InvalidArgumentUsage::what() const noexcept {
+  return message.c_str();
+}
 
 // invalid configuration file
 
-InvalidConfigurationFile::InvalidConfigurationFile(const std::string lookup) { message = "Could not find " + lookup + ".\nPlease refer to docs for correct configuration file: https://github.com/sebvu/workout-tracker"; }
+InvalidConfigurationFile::InvalidConfigurationFile(const std::string lookup) {
+  message = "Could not find path " + lookup +
+            ".\nPlease refer to docs for correct configuration file: "
+            "https://github.com/sebvu/workout-tracker";
+}
 
-const char *InvalidConfigurationFile::what() const noexcept { return message.c_str(); }
+const char *InvalidConfigurationFile::what() const noexcept {
+  return message.c_str();
+}

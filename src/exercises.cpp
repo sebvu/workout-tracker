69d3e54968247fda23f4cb5f1a0025ea8e59345e
sebvu <santosjester738@gmail.com>
2024-12-21 16:17:59 -0600

    Create skeleton methods for exercises.cpp

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -1,3 +1,40 @@
 #include "exercises.hpp"
+#include <iostream>
 
-void exercises::test() { std::cout << "the test worked marvelously" << std::endl; }
+/* Exercise */
+
+std::optional<std::vector<std::string>>
+Exercise::parseOptionalVectorArray(toml::array *arr) {}
+
+Exercise::Exercise(toml::table &exercise) {}
+
+std::string Exercise::getName() const {}
+
+const std::optional<std::vector<std::string>> &
+Exercise::getPrimaryMuscles() const {}
+
+const std::optional<std::vector<std::string>> &
+Exercise::getSecondaryMuscles() const {}
+
+std::string Exercise::getCommentary() const {}
+
+const std::optional<std::vector<std::string>> &
+Exercise::getInstructions() const {}
+
+/* ExerciseCategory */
+
+ExerciseCategory::ExerciseCategory(toml::table &category) {}
+
+int ExerciseCategory::getTotalExercise() const {}
+
+const std::vector<Exercise> &ExerciseCategory::getExerciseList() const {}
+
+void ExerciseCategory::printExercises() const {}
+
+/* Exercises */
+
+Exercises::Exercises(toml::table &config) {}
+
+int Exercises::getTotalExercise() const {}
+const std::vector<ExerciseCategory> &Exercises::getExerciseCategories() const {}
+void Exercises::printCategories() const {}

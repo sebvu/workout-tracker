0c3ab95b3eb41f091dce70ea4996389d92ba256b
sebvu <santosjester738@gmail.com>
2024-12-21 20:43:16 -0600

    Remove current implementation for clean slate

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -3,38 +3,6 @@
 
 /* Exercise */
 
-std::optional<std::vector<std::string>>
-Exercise::parseOptionalVectorArray(toml::array *arr) {}
-
-Exercise::Exercise(toml::table &exercise) {}
-
-std::string Exercise::getName() const {}
-
-const std::optional<std::vector<std::string>> &
-Exercise::getPrimaryMuscles() const {}
-
-const std::optional<std::vector<std::string>> &
-Exercise::getSecondaryMuscles() const {}
-
-std::string Exercise::getCommentary() const {}
-
-const std::optional<std::vector<std::string>> &
-Exercise::getInstructions() const {}
-
 /* ExerciseCategory */
 
-ExerciseCategory::ExerciseCategory(toml::table &category) {}
-
-int ExerciseCategory::getTotalExercise() const {}
-
-const std::vector<Exercise> &ExerciseCategory::getExerciseList() const {}
-
-void ExerciseCategory::printExercises() const {}
-
 /* Exercises */
-
-Exercises::Exercises(toml::table &config) {}
-
-int Exercises::getTotalExercise() const {}
-const std::vector<ExerciseCategory> &Exercises::getExerciseCategories() const {}
-void Exercises::printCategories() const {}

1e8551755bbdf552bd488024df7f5f16c8e34798
sebvu <santosjester738@gmail.com>
2024-12-23 20:41:00 -0600

    Create exerciseEnums file

---
+++ include/exerciseEnums.hpp
@@ -0,0 +1,19 @@
+#ifndef EXERCISEENUMS_HPP
+#define EXERCISEENUMS_HPP
+
+enum MuscleGroup {
+  Chest,
+  Shoulder,
+  Biceps,
+  Triceps,
+  Legs,
+  Back,
+  Glute,
+  Abs,
+  Calves,
+  Forearm_Flexors,
+  Forearm_Extensors,
+  Cardio_Training
+};
+
+#endif  // EXERCISEENUMS_HPP
